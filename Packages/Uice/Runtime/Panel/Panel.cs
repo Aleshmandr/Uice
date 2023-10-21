129fb6639b6ae2e683c67e840439f1fbac0a6c8f
Alexander Papchikhin <aleshmandr@gmail.com>
2023-10-22 00:19:56 +0200

    Samples meta remove, UIEnvironment remove

--- Packages/Uice/Runtime/Panel/Panel.cs
+++ Packages/Uice/Runtime/Panel/Panel.cs
@@ -2,17 +2,13 @@
 
 namespace Uice
 {
-	public abstract class Panel : Panel<NullContext>
-	{
+    public abstract class Panel : Panel<NullContext> { }
 
-	}
+    public abstract class Panel<T> : View<T>, IPanel where T : IContext
+    {
+        public PanelPriority Priority => priority;
 
-	public abstract class Panel<T> : View<T>, IPanel
-		where T : IContext
-	{
-		public PanelPriority Priority => priority;
-
-		[Header("Panel Properties")]
-		[SerializeField] private PanelPriority priority;
-	}
+        [Header("Panel Properties")] [SerializeField]
+        private PanelPriority priority;
+    }
 }
