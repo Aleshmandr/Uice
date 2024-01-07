50725ad9f2aafa4e9867e5f6abca46b12e9c9800
Aleshmandr <aleshmandr@gmail.com>
2024-01-07 22:33:37 +0100

    Animator transition

---
+++ Packages/Uice/Runtime/Transition/AnimatorTransition.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Uice
+{
+    public class AnimatorTransition : ComponentTransition
+    {
+        [SerializeField] private string stateName;
+        
+        protected override void PrepareInternal(Transform target)
+        {
+            
+        }
+
+        protected override async Task AnimateInternal(Transform target)
+        {
+            if (target.TryGetComponent(out Animator animator))
+            {
+                animator.Play(stateName);
+
+                while (true)
+                {
+                    await Task.Yield();
+                    var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+                    if (!stateInfo.IsName(stateName))
+                    {
+                        continue;
+                    }
+
+                    if (stateInfo.normalizedTime >= 1f)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        protected override void CleanupInternal(Transform target)
+        {
+            
+        }
+    }
+}
