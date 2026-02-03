2438130eaee02ba60caba6d0d3699ad621a75aa7
Alexander Papchikhin <alexander@Alexanders-MacBook-Pro.local>
2026-02-03 17:09:14 +0100

    Widget visibility state events fix, refactoring

--- Packages/Uice/Runtime/Widget/Widget.cs
+++ Packages/Uice/Runtime/Widget/Widget.cs
@@ -6,197 +6,187 @@ using UnityEngine.Events;
 
 namespace Uice
 {
-	[Serializable]
-	public class WidgetEvents
-	{
-		public UnityEvent OnShowing = new UnityEvent();
-		public UnityEvent OnShown = new UnityEvent();
-		public UnityEvent OnHiding = new UnityEvent();
-		public UnityEvent OnHidden = new UnityEvent();
-	}
+    [Serializable]
+    public class WidgetEvents
+    {
+        public UnityEvent OnShowing = new UnityEvent();
+        public UnityEvent OnShown = new UnityEvent();
+        public UnityEvent OnHiding = new UnityEvent();
+        public UnityEvent OnHidden = new UnityEvent();
+    }
 
-	public enum WidgetState
-	{
-		None,
-		Showing,
-		Idle,
-		Hiding,
-		Closed
-	}
+    public enum WidgetState : byte
+    {
+        None,
+        Showing,
+        Idle,
+        Hiding,
+        Closed
+    }
 
-	[RequireComponent(typeof(RectTransform))]
-	public class Widget : MonoBehaviour, ITransitionable
-	{
-		public event TransitionableEventHandler Showing;
-		public event TransitionableEventHandler Shown;
-		public event TransitionableEventHandler Hiding;
-		public event TransitionableEventHandler Hidden;
+    [RequireComponent(typeof(RectTransform))]
+    public class Widget : MonoBehaviour, ITransitionable
+    {
+        public event TransitionableEventHandler Showing;
+        public event TransitionableEventHandler Shown;
+        public event TransitionableEventHandler Hiding;
+        public event TransitionableEventHandler Hidden;
 
-		public bool IsVisible { get; private set; }
-		public WidgetState State { get; private set; } = WidgetState.None;
-		public bool IsPlayingAnimation => State == WidgetState.Showing || State == WidgetState.Hiding;
+        public bool IsVisible { get; private set; }
+        public WidgetState State { get; private set; } = WidgetState.None;
+        public bool IsPlayingAnimation => State == WidgetState.Showing || State == WidgetState.Hiding;
 
-		public ComponentTransition ShowTransition
-		{
-			get => showTransition;
-			set => showTransition = value;
-		}
+        public ComponentTransition ShowTransition
+        {
+            get => showTransition;
+            set => showTransition = value;
+        }
 
-		public ComponentTransition HideTransition
-		{
-			get => hideTransition;
-			set => hideTransition = value;
-		}
+        public ComponentTransition HideTransition
+        {
+            get => hideTransition;
+            set => hideTransition = value;
+        }
 
-		[Header("Transitions")]
-		[SerializeField] private ComponentTransition showTransition = default;
-		[SerializeField] private ComponentTransition hideTransition = default;
-		[SerializeField] private WidgetEvents transitionEvents = new WidgetEvents();
+        [Header("Transitions")] [SerializeField]
+        private ComponentTransition showTransition = default;
 
-		private readonly TransitionHandler transitionHandler = new TransitionHandler();
-		private RectTransform rectTransform;
-		private bool isInitialized;
+        [SerializeField] private ComponentTransition hideTransition = default;
+        [SerializeField] private WidgetEvents transitionEvents = new WidgetEvents();
 
-		protected virtual void Awake()
-		{
-			EnsureInitialState();
-		}
+        private readonly TransitionHandler transitionHandler = new TransitionHandler();
+        private RectTransform rectTransform;
+        private bool isInitialized;
 
-		public virtual async Task Show(ITransition overrideTransition = null)
-		{
-			EnsureInitialState();
-			
-			State = WidgetState.Showing;
-			OnShowing();
+        protected virtual void Awake()
+        {
+            EnsureInitialState();
+        }
 
-			if (IsVisible == false)
-			{
-				IsVisible = true;
-				
-				try
-				{
-					ITransition transition = overrideTransition ?? ShowTransition;
-					
-					await transitionHandler.Show(rectTransform, transition);
-				}
-				catch(Exception e)
-				{
-					Debug.Log(e);
-				}
-			}
-			
-			State = WidgetState.Idle;
-			OnShown();
-		}
+        public virtual async Task Show(ITransition overrideTransition = null)
+        {
+            EnsureInitialState();
 
-		public virtual async Task Hide(ITransition overrideTransition = null)
-		{
-			EnsureInitialState();
-			
-			State = WidgetState.Hiding;
-			OnHiding();
+            if (IsVisible)
+            {
+                return;
+            }
 
-			if (IsVisible)
-			{
-				IsVisible = false;
-				
-				try
-				{
-					ITransition transition = overrideTransition ?? HideTransition;
-					
-					await transitionHandler.Hide(rectTransform, transition);
-				}
-				catch(Exception e)
-				{
-					Debug.Log(e);
-				}
-			}
+            State = WidgetState.Showing;
+            OnShowing();
+            IsVisible = true;
 
-			State = WidgetState.Closed;
-			OnHidden();
-		}
-		
-		public ITransition GetShowTransition(TransitionData transitionData)
-		{
-			ITransition transition = default;
+            try
+            {
+                ITransition transition = overrideTransition ?? ShowTransition;
+                await transitionHandler.Show(rectTransform, transition);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
 
-			if (showTransition is TransitionPicker transitionPicker)
-			{
-				transition = transitionPicker.SelectTransition(transitionData);
-			}
-			else
-			{
-				transition = showTransition;
-			}
+            State = WidgetState.Idle;
+            OnShown();
+        }
 
-			return transition;
-		}
+        public virtual async Task Hide(ITransition overrideTransition = null)
+        {
+            EnsureInitialState();
 
-		public ITransition GetHideTransition(TransitionData transitionData)
-		{
-			ITransition transition = default;
+            if (!IsVisible)
+            {
+                return;
+            }
 
-			if (hideTransition is TransitionPicker transitionPicker)
-			{
-				transition = transitionPicker.SelectTransition(transitionData);
-			}
-			else
-			{
-				transition = hideTransition;
-			}
+            State = WidgetState.Hiding;
+            OnHiding();
+            IsVisible = false;
 
-			return transition;
-		}
+            try
+            {
+                ITransition transition = overrideTransition ?? HideTransition;
+                await transitionHandler.Hide(rectTransform, transition);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
 
-		public void ShowWidget()
-		{
-			Show().RunAndForget();
-		}
+            State = WidgetState.Closed;
+            OnHidden();
+        }
 
-		public void HideWidget()
-		{
-			Hide().RunAndForget();
-		}
+        public ITransition GetShowTransition(TransitionData transitionData)
+        {
+            if (showTransition is TransitionPicker transitionPicker)
+            {
+                return  transitionPicker.SelectTransition(transitionData);
+            }
 
-		protected void EnsureInitialState()
-		{
-			if (isInitialized == false)
-			{
-				isInitialized = true;
+            return showTransition;
+        }
 
-				Initialize();
-			}
-		}
+        public ITransition GetHideTransition(TransitionData transitionData)
+        {
+            if (hideTransition is TransitionPicker transitionPicker)
+            {
+                return transitionPicker.SelectTransition(transitionData);
+            }
 
-		protected virtual void Initialize()
-		{
-			rectTransform = GetComponent<RectTransform>();
-			IsVisible = gameObject.activeSelf;
-			State = IsVisible ? WidgetState.Idle : WidgetState.Closed;
-		}
+            return hideTransition;
+        }
 
-		protected virtual void OnShowing()
-		{
-			Showing?.Invoke(this);
-			transitionEvents.OnShowing.Invoke();
-		}
+        public void ShowWidget()
+        {
+            Show().RunAndForget();
+        }
 
-		protected virtual void OnShown()
-		{
-			Shown?.Invoke(this);
-			transitionEvents.OnShown.Invoke();
-		}
+        public void HideWidget()
+        {
+            Hide().RunAndForget();
+        }
 
-		protected virtual void OnHiding()
-		{
-			Hiding?.Invoke(this);
-			transitionEvents.OnHiding.Invoke();
-		}
+        protected void EnsureInitialState()
+        {
+            if (isInitialized)
+            {
+                return;
+            }
 
-		protected virtual void OnHidden()
-		{
-			Hidden?.Invoke(this);
-			transitionEvents.OnHidden.Invoke();
-		}
-	}
+            Initialize();
+            isInitialized = true;
+        }
+
+        protected virtual void Initialize()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            IsVisible = gameObject.activeSelf;
+            State = IsVisible ? WidgetState.Idle : WidgetState.Closed;
+        }
+
+        protected virtual void OnShowing()
+        {
+            Showing?.Invoke(this);
+            transitionEvents.OnShowing.Invoke();
+        }
+
+        protected virtual void OnShown()
+        {
+            Shown?.Invoke(this);
+            transitionEvents.OnShown.Invoke();
+        }
+
+        protected virtual void OnHiding()
+        {
+            Hiding?.Invoke(this);
+            transitionEvents.OnHiding.Invoke();
+        }
+
+        protected virtual void OnHidden()
+        {
+            Hidden?.Invoke(this);
+            transitionEvents.OnHidden.Invoke();
+        }
+    }
 }
